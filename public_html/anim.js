05e8c4f90ca4aaf965b82a763cfb289aca60ed09
UTILISATEUR <UTILISATEUR@192.168.1.167>
2017-07-25 14:24:43 +0200

    presque fini !

--- public_html/anim.js
+++ public_html/anim.js
@@ -1,20 +1,24 @@
-function main(min, max){
+function main(min, max) {
     var jeu = prompt("Devine un nombre en 10 et 20 !");
-    
-    var cpt = 0 ;
-
-    var nb = min + (max-min+1)*Math.random();
+    var parti = 0;
+    var cpt = 0;
+    var continuer;
+    var nb = min + (max - min + 1) * Math.random();
     var deci = Math.floor(nb);
     console.log(deci);
-    
-    do{
-        cpt++;
-        if(jeu >= deci){
-            jeu = prompt("C'est moin!");
-        }
-        else if(jeu <= deci){
-            jeu = prompt("C'est plus!");
-        }
-    }while(jeu != deci)
-    alert("Gagné en "+cpt+" essai !");
+
+    do {
+        do {
+            cpt++;
+            if (jeu >= deci) {
+                jeu = prompt("C'est moin!");
+            } else if (jeu <= deci) {
+                jeu = prompt("C'est plus!");
+            }
+        } while (jeu != deci)
+        parti++;
+        continuer = confirm("Gagné en " + cpt + " essai et " + parti + " parties .\nVeux-tu rejouer ?");
+    } while (continuer == true);
+    alert("the end");
 }
+
