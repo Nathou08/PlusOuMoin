b3d04d7d4209f5e7e87e0381dd41c669bcdbcd03
UTILISATEUR <UTILISATEUR@192.168.1.167>
2017-07-25 11:29:18 +0200

    Avancement

--- public_html/anim.js
+++ public_html/anim.js
@@ -1,3 +1,17 @@
-function come(){
+function main(min, max){
     var jeu = prompt("Devine un nombre en 10 et 20 !");
+
+    var nb = min + (max-min+1)*Math.random();
+    var deci = Math.floor(nb);
+    console.log(deci);
+    
+    do{
+        if(jeu >= deci){
+            jeu = prompt("C'est moin!");
+        }
+        else if(jeu <= deci){
+            jeu = prompt("C'est plus!");
+        }
+    }while(jeu != deci)
+    alert("Gagné!");
 }
